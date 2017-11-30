7225be95999e956c6a6a1a20c84ca274dfba01ed
alex-precosky <precosky@mac.com>
2017-11-29 21:57:35 -0800

    Roughed out packet encode/decode. Didn't do checksums yet

---
+++ esp_fw/lib/LEDPanel_Serial_Comm/src/LEDPanel_Serial_Comm.h
@@ -0,0 +1,15 @@
+// Encodes and decodes packets for sending between the ESP8266 and the NIOS II
+
+#ifndef __LEDPANEL_SERIAL_COMM_H__
+
+    #define LEDPANEL_COMM_START_BYTE 0x00
+
+    #define LEDPANEL_COMM_CHECKSUM_ERROR 0x01
+    #define LEDPANEL_COMM_START_BYTE_ERROR 0x01
+
+    // Encoded packet will be stored in dest. It must be as big as the payload plus
+    // 9 bytes for the start byte, four length bytes, and a four byte checksum
+    void encode_serial_packet(char* dest, char* payload, int payload_size);
+    void decode_serial_packet(char* payload, char* packet);
+
+#endif
