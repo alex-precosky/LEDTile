ead1aef0e2dcb2b78e6664cd574f70fcace60f77
Alex Precosky <precosky@amazon.com>
2019-05-26 18:09:58 -0700

    Commented the flash_programmer.sh script better

--- script/flash_programmer.sh
+++ script/flash_programmer.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 #
-# This file was automatically generated.
-#
-# It can be overwritten by nios2-flash-programmer-generate or nios2-flash-programmer-gui.
-#
+# This script is to program the FPGA configuration and Nios II software into the
+# Terasic DE0 Nano board for the LED tile
 
 #
-# Converting SOF File: C:\Users\Alex\Desktop\LEDTile_git_export_non_bare\output_files\LEDTile.sof to: "..\flash/LEDTile_epcs_flash_controller.flash"
+# Converting SOF file to a .flash file. This contains the FPGA logic
 #
 sof2flash --input="../FPGA/output_files/LEDTile.sof" --output="../FPGA/flash/LEDTile_epcs_flash_controller.flash" --epcs --verbose 
 
 #
-# Programming File: "..\flash/LEDTile_epcs_flash_controller.flash" To Device: epcs_flash_controller
+# Programming the .flash file now that it's converted
+# base: base address of the flash memory. The .flash file will take care of the correct offsets into this
+# sidp: address of the System ID component
 #
 nios2-flash-programmer "../FPGA/flash/LEDTile_epcs_flash_controller.flash" --base=0x4009000 --epcs --sidp=0x400A0F0 --id=0x0 --accept-bad-sysid --device=1 --instance=0 '--cable=USB-Blaster on localhost [USB-0]' --program --verbose 
 
 #
-# Converting ELF File: C:\Users\Alex\Desktop\LEDTile_git_export_non_bare\software\LEDTileSerialAPIApplication\LEDTileSerialAPIApplication.elf to: "..\flash/LEDTileSerialAPIApplication_epcs_flash_controller.flash"
+# Converting ELF File to a .flash file. This contains the software for the Nios II CPU
 #
 elf2flash --input="../NIOS_II_Software/LEDTileSerialAPIApplication/LEDTileSerialAPIApplication.elf" --output="../FPGA/flash/LEDTileSerialAPIApplication_epcs_flash_controller.flash" --epcs --after="../FPGA/flash/LEDTile_epcs_flash_controller.flash" --verbose 
 
 #
-# Programming File: "..\flash/LEDTileSerialAPIApplication_epcs_flash_controller.flash" To Device: epcs_flash_controller
+# Programming the .flash file now that it's converted
 #
 nios2-flash-programmer "../FPGA/flash/LEDTileSerialAPIApplication_epcs_flash_controller.flash" --base=0x4009000 --epcs --sidp=0x400A0F0 --id=0x0 --accept-bad-sysid --device=1 --instance=0 '--cable=USB-Blaster on localhost [USB-0]' --program --verbose 
 
